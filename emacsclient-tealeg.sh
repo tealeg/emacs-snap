4b136758fde4a2c7166f1c3f2fdca93b2a436206
Geoffrey J. Teale <geoffrey.teale@canonical.com>
2016-08-11 12:28:14 +0200

    First useful version.

--- emacsclient-tealeg.sh
+++ emacsclient-tealeg.sh
@@ -3,4 +3,4 @@
 export EMACSDATA=$SNAP/share/emacs/24.5/etc
 export EMACSLOADPATH=$SNAP/share/emacs/site-lisp:$SNAP/share/emacs/24.5/lisp/
 export INFOPATH=$SNAP/share/info/
-desktop-launch emacs "$@"
+desktop-launch emacsclient "$@"
