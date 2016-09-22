32a97fc83233473e5085d22b784913997138a646
Geoffrey J. Teale <geoffrey.tealeg@canonical.com>
2016-09-22 23:02:29 +0200

    Emacs 25.1 support

--- scripts/emacs-tealeg.sh
+++ scripts/emacs-tealeg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-EMACSVERSION=25.1.50
+EMACSVERSION=25.1
 export EMACSDATA=$SNAP/share/emacs/$EMACSVERSION/etc
 export EMACSLOADPATH=$SNAP/share/emacs/site-lisp:$SNAP/share/emacs/$EMACSVERSION/lisp/
 export INFOPATH=$SNAP/share/info/
